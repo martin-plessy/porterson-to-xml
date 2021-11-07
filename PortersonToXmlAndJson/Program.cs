139cb1b434205db93779d737ac1e8acd078fe445
Martin Plessy <plessy.martin@outlook.com>
2021-11-07 15:21:45 +0000

    Part 3. Brute solution
    Untested

--- PortersonToXmlAndJson/Program.cs
+++ PortersonToXmlAndJson/Program.cs
@@ -1,26 +1,63 @@
-﻿using System;
+using System;
+using System.IO;
+using System.Text;
 
 namespace PortersonToXmlAndJson
 {
-    class Program
+    internal static class Program
     {
-        private static void TransformAndSaveToXml(string input, string output)
+        internal static int Main(string[] args)
         {
-            throw new NotImplementedException();
-        }
-        static void TransformAndSaveToJson(string input, string output)
-        {
-            throw new NotImplementedException();
-        }
-        static void Main(string[] args)
-        {
+            if (args.Length != 3)
+            {
+                Console.Error.WriteLine("Usage: PortersonToJson <input:FILE_PATH> <xml_output:FILE_PATH> <json_output:FILE_PATH>");
+                return 1;
+            }
+
+            var inputPath = args[0];
+            var xmlOutputPath = args[1];
+            var jsonOutputPath = args[2];
+
+            if (!File.Exists(inputPath))
+            {
+                Console.Error.WriteLine("The input file does not exist...");
+                return 2;
+            }
+
+            if (Directory.Exists(xmlOutputPath) || Directory.Exists(jsonOutputPath))
+            {
+                Console.Error.WriteLine("An output path points to an existing directory...");
+                return 3;
+            }
+
             Console.WriteLine("To Both");
 
-            var input = @"PortersonToXml\ExampleData\ExampleInput.xml";
-            var xmlOutput = @"PortersonToXml\ExampleData\ExampleOutputBoth.xml";
-            var jsonOutput = @"PortersonToXml\ExampleData\ExampleOutputBoth.json";
-            TransformAndSaveToXml(input, xmlOutput);
-            TransformAndSaveToJson(input, jsonOutput);
+            TransformAndSaveToXml(inputPath, xmlOutputPath);
+            TransformAndSaveToJson(inputPath, jsonOutputPath);
+
+            return 0;
+        }
+
+        private static void TransformAndSaveToXml(string inputPath, string outputPath)
+        {
+            var input = File.ReadAllText(inputPath);
+
+            var _1 = PortersonToXml.Services.DataProcessor.Deserialize(input);
+            var _2 = PortersonToXml.Services.DataProcessor.Transform(_1);
+            var output = PortersonToXml.Services.DataProcessor.Serialize(_2);
+
+            File.WriteAllText(outputPath, output, Encoding.UTF8);
+        }
+
+        private static void TransformAndSaveToJson(string inputPath, string outputPath)
+        {
+            var input = File.ReadAllText(inputPath);
+
+            var _1 = PortersonToJson.Services.DataProcessor.Deserialize(input);
+            var _2 = PortersonToJson.Services.DataProcessor.Transform(_1);
+            var output = PortersonToJson.Services.DataProcessor.Serialize(_2);
+
+            File.WriteAllText(outputPath, output, Encoding.UTF8);
         }
     }
 }
